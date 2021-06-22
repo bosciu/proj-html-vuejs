1e0d3e0221410b7e86d6b9aa0b765f628b893fed
bosciu <bosciu@icloud.com>
2021-06-22 10:51:36 +0200

    Completamento seconda sezione

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -26,6 +26,46 @@
 			</div>
 		</section>
 		<!-- /Prima sezione con informazioni principali -->
+		<!-- Seconda sezione con informazioni secondarie -->
+		<section class="row" id="secondary-informations">
+			<div class="col-6" id="information">
+				<h2>Get tips tricks on how to skyrocket your sales.</h2>
+				<p>
+					Faff about only a quid blower i don't want no aggro bleeding
+					chimney pot burke tosser cras nice one boot fanny.!
+				</p>
+				<div id="reporting-analysis" class="d-flex align-items-center">
+					<div class="img-container">
+						<img src="../../assets/images/510.png" alt="" />
+					</div>
+					<div class="text-container">
+						<h3>Reporting Analysis</h3>
+						<p>
+							It is a long established fact that a reader will be
+							distracted by the readable content of a page when
+							looking at is layout.
+						</p>
+					</div>
+				</div>
+				<div id="technical-seo" class="d-flex align-items-center">
+					<div class="img-container">
+						<img src="../../assets/images/511.png" alt="" />
+					</div>
+					<div class="text-container">
+						<h3>Technical SEO Audit</h3>
+						<p>
+							It is a long established fact that a reader will be
+							distracted by the readable content of a page when
+							looking at is layout.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-6" id="img-container">
+				<img src="../../assets/images/509.jpeg" alt="" />
+			</div>
+		</section>
+		<!-- /Seconda sezione con informazioni secondarie -->
 	</main>
 </template>
 
@@ -38,6 +78,7 @@ export default {
 <style lang="scss" scoped>
 @import "../../styles/variables.scss";
 #main-informations {
+	margin-bottom: $sectionMargin;
 	#img-container {
 		img {
 			width: 100%;
@@ -57,6 +98,34 @@ export default {
 					color: $buttonPrimary;
 				}
 			}
+		}
+	}
+}
+#secondary-informations {
+	& > #information {
+		h3 {
+			font-size: 20px;
+		}
+		& > p {
+			margin: 15px 0 50px;
+		}
+		#reporting-analysis,
+		#technical-seo {
+			padding: 25px 70px 25px 10px;
+			.img-container {
+				margin-right: 10px;
+			}
+			h3 {
+				color: $buttonPrimary;
+				font-weight: 500;
+			}
+			p {
+				margin-bottom: 0;
+				font-weight: 500;
+			}
+		}
+		#reporting-analysis {
+			box-shadow: 0 25px 30px rgba(0, 0, 0, 10%);
 		}
 	}
 }

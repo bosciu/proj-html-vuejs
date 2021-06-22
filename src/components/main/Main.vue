4ea0b49d5fb6c9f3f8a46c0d42f3b7674c21111a
bosciu <bosciu@icloud.com>
2021-06-22 11:50:54 +0200

    Completata sezione Features

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -1,77 +1,183 @@
 <template>
-	<main class="container">
-		<!-- Prima sezione con informazioni principali -->
-		<section class="row" id="main-informations">
-			<div
-				class="d-flex col-4"
-				v-for="(infoCard, index) in 3"
-				:key="index"
-			>
-				<div id="img-container">
-					<img src="../../assets/images/24.png" alt="PC" />
+	<main>
+		<div class="container">
+			<!-- Prima sezione con informazioni principali -->
+			<section class="row" id="main-informations">
+				<div
+					class="d-flex col-4"
+					v-for="(infoCard, index) in 3"
+					:key="index"
+				>
+					<div id="img-container">
+						<img src="../../assets/images/24.png" alt="PC" />
+					</div>
+					<div id="informations">
+						<h3>Online Marketing</h3>
+						<p>
+							It is a long established fact that a reader will be
+							distracted.
+						</p>
+						<ul>
+							<li><i class="fas fa-check"></i>SEO</li>
+							<li><i class="fas fa-check"></i>SEM</li>
+							<li>
+								<i class="fas fa-check"></i>Website Strategy
+							</li>
+							<li>
+								<i class="fas fa-check"></i>Social management
+							</li>
+						</ul>
+					</div>
 				</div>
-				<div id="informations">
-					<h3>Online Marketing</h3>
+			</section>
+			<!-- /Prima sezione con informazioni principali -->
+			<!-- Seconda sezione con informazioni secondarie -->
+			<section class="row" id="secondary-informations">
+				<div class="col-6" id="information">
+					<h2>Get tips tricks on how to skyrocket your sales.</h2>
 					<p>
-						It is a long established fact that a reader will be
-						distracted.
+						Faff about only a quid blower i don't want no aggro
+						bleeding chimney pot burke tosser cras nice one boot
+						fanny.!
 					</p>
-					<ul>
-						<li><i class="fas fa-check"></i>SEO</li>
-						<li><i class="fas fa-check"></i>SEM</li>
-						<li><i class="fas fa-check"></i>Website Strategy</li>
-						<li><i class="fas fa-check"></i>Social management</li>
-					</ul>
+					<div
+						id="reporting-analysis"
+						class="d-flex align-items-center"
+					>
+						<div class="img-container">
+							<img src="../../assets/images/510.png" alt="" />
+						</div>
+						<div class="text-container">
+							<h3>Reporting Analysis</h3>
+							<p>
+								It is a long established fact that a reader will
+								be distracted by the readable content of a page
+								when looking at is layout.
+							</p>
+						</div>
+					</div>
+					<div id="technical-seo" class="d-flex align-items-center">
+						<div class="img-container">
+							<img src="../../assets/images/511.png" alt="" />
+						</div>
+						<div class="text-container">
+							<h3>Technical SEO Audit</h3>
+							<p>
+								It is a long established fact that a reader will
+								be distracted by the readable content of a page
+								when looking at is layout.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-6" id="img-container">
+					<img src="../../assets/images/509.jpeg" alt="" />
+				</div>
+			</section>
+			<!-- /Seconda sezione con informazioni secondarie -->
+			<!-- Terza sezione Features -->
+		</div>
+		<section id="features">
+			<img
+				src="../../assets/images/14.png"
+				alt="background"
+				class="features-background"
+			/>
+			<div class="container pe-5">
+				<div
+					class="
+						heading
+						d-flex
+						align-items-center
+						justify-content-between
+					"
+				>
+					<div class="title">
+						<h2>Popular features that your business needs</h2>
+						<p>
+							Faff about only a quid blower i don't want no agro
+							bleeding chimney pot burke tosser cras nice one boot
+							fanny.!
+						</p>
+					</div>
+					<Button msg="Explore more features" />
+				</div>
+				<div class="features-container">
+					<div
+						class="card"
+						v-for="(feature, index) in features"
+						:key="index"
+					>
+						<h4>
+							{{ feature.id }}
+						</h4>
+						<img
+							:src="
+								require('../../assets/images' + feature.imgPath)
+							"
+							alt=""
+						/>
+					</div>
 				</div>
 			</div>
 		</section>
-		<!-- /Prima sezione con informazioni principali -->
-		<!-- Seconda sezione con informazioni secondarie -->
-		<section class="row" id="secondary-informations">
-			<div class="col-6" id="information">
-				<h2>Get tips tricks on how to skyrocket your sales.</h2>
-				<p>
-					Faff about only a quid blower i don't want no aggro bleeding
-					chimney pot burke tosser cras nice one boot fanny.!
-				</p>
-				<div id="reporting-analysis" class="d-flex align-items-center">
-					<div class="img-container">
-						<img src="../../assets/images/510.png" alt="" />
-					</div>
-					<div class="text-container">
-						<h3>Reporting Analysis</h3>
-						<p>
-							It is a long established fact that a reader will be
-							distracted by the readable content of a page when
-							looking at is layout.
-						</p>
-					</div>
-				</div>
-				<div id="technical-seo" class="d-flex align-items-center">
-					<div class="img-container">
-						<img src="../../assets/images/511.png" alt="" />
-					</div>
-					<div class="text-container">
-						<h3>Technical SEO Audit</h3>
-						<p>
-							It is a long established fact that a reader will be
-							distracted by the readable content of a page when
-							looking at is layout.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-6" id="img-container">
-				<img src="../../assets/images/509.jpeg" alt="" />
-			</div>
-		</section>
-		<!-- /Seconda sezione con informazioni secondarie -->
+		<!-- /Terza sezione Features -->
 	</main>
 </template>
 
 <script>
+import Button from "../Button";
 export default {
-	name: "Main"
+	name: "Main",
+	components: {
+		Button
+	},
+	data() {
+		return {
+			features: [
+				{
+					id: "Customized Invoices",
+					imgPath: "/financial-presentation.png",
+					link: "#"
+				},
+				{
+					id: "Stock Management",
+					imgPath: "/paper-plane.png",
+					link: "#"
+				},
+				{
+					id: "Receivable & Paybles",
+					imgPath: "/bill.png",
+					link: "#"
+				},
+				{
+					id: "Manage Buying",
+					imgPath: "/financial-presentation.png",
+					link: "#"
+				},
+				{
+					id: "Powerful & Secure",
+					imgPath: "/paper-plane.png",
+					link: "#"
+				},
+				{
+					id: "Fastest Return Filing",
+					imgPath: "/bill.png",
+					link: "#"
+				},
+				{
+					id: "Monthly Detailed",
+					imgPath: "/financial-presentation.png",
+					link: "#"
+				},
+				{
+					id: "Product Management",
+					imgPath: "/paper-plane.png",
+					link: "#"
+				}
+			]
+		};
+	}
 };
 </script>
 
@@ -102,6 +208,7 @@ export default {
 	}
 }
 #secondary-informations {
+	margin-bottom: $sectionMargin;
 	& > #information {
 		h3 {
 			font-size: 20px;
@@ -124,8 +231,44 @@ export default {
 				font-weight: 500;
 			}
 		}
+		#technical-seo {
+			margin-top: 15px;
+			h3 {
+				color: black;
+			}
+		}
 		#reporting-analysis {
 			box-shadow: 0 25px 30px rgba(0, 0, 0, 10%);
+		}
+	}
+}
+#features {
+	position: relative;
+	& > img {
+		position: absolute;
+		right: 87%;
+		bottom: 0;
+		z-index: -10;
+	}
+	&::after {
+		content: "";
+		display: table;
+		clear: both;
+	}
+	.title {
+		p {
+			width: 65%;
+		}
+	}
+	.card {
+		width: calc(100% / 4 - 30px);
+		margin: 15px;
+		padding: 25px 20px 40px;
+		border: none;
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 10%);
+		float: left;
+		img {
+			width: 20%;
 		}
 	}
 }
